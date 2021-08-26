db9eb05ef552dace33608d0c12a335c4863d3286
Struan Cooper <struancooper4@hotmail.com>
2021-08-26 15:22:27 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace CovidRepo {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Gender = new System.Windows.Forms.ComboBox();
+            this.LastName = new System.Windows.Forms.TextBox();
+            this.FirstName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Province = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ID
+            // 
+            this.ID.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.Location = new System.Drawing.Point(296, 135);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(242, 30);
+            this.ID.TabIndex = 15;
+            this.ID.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Gill Sans MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(129, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(542, 48);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Covid Vacsination Registeration";
+            // 
+            // Gender
+            // 
+            this.Gender.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gender.FormattingEnabled = true;
+            this.Gender.Items.AddRange(new object[] {
+            "Male ",
+            "Female ",
+            "Other"});
+            this.Gender.Location = new System.Drawing.Point(296, 243);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(242, 35);
+            this.Gender.TabIndex = 13;
+            this.Gender.Text = "Gender";
+            // 
+            // LastName
+            // 
+            this.LastName.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastName.Location = new System.Drawing.Point(296, 207);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(242, 30);
+            this.LastName.TabIndex = 12;
+            this.LastName.Text = "Last Name";
+            // 
+            // FirstName
+            // 
+            this.FirstName.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstName.Location = new System.Drawing.Point(296, 171);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(242, 30);
+            this.FirstName.TabIndex = 11;
+            this.FirstName.Text = "First Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(291, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 27);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Please Enter Your Information";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Gill Sans MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(352, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Province
+            // 
+            this.Province.Font = new System.Drawing.Font("Gill Sans MT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Province.FormattingEnabled = true;
+            this.Province.Items.AddRange(new object[] {
+            "Free State",
+            "Gauteng",
+            "KwaZulu-Natal",
+            "Limpopo",
+            "Mpumalanga",
+            "Northern Cape",
+            "North West",
+            "Eastern Cape",
+            "Western Cape"});
+            this.Province.Location = new System.Drawing.Point(296, 284);
+            this.Province.Name = "Province";
+            this.Province.Size = new System.Drawing.Size(242, 35);
+            this.Province.TabIndex = 8;
+            this.Province.Text = "Province";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Province);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox Gender;
+        private System.Windows.Forms.TextBox LastName;
+        private System.Windows.Forms.TextBox FirstName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox Province;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CovidRepo {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private static string myConnection = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+        SqlConnection con = new SqlConnection(myConnection);
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            string first = Convert.ToString(FirstName.Text);
+            string last = Convert.ToString(LastName.Text);
+            string gender = Convert.ToString(Gender.Text);
+            string province = Convert.ToString(Province.Text);
+
+            string insertQuery = "Insert Into CovidRegistration Values (@ID, @First, @Last, @Gender, @Province)";
+
+            SqlCommand cmd = new SqlCommand(insertQuery, con);
+            cmd.Parameters.AddWithValue("@ID", ID.Text);
+            cmd.Parameters.AddWithValue("@First", FirstName.Text);
+            cmd.Parameters.AddWithValue("@Last", LastName.Text);
+            cmd.Parameters.AddWithValue("@Gender", Gender.Text);
+            cmd.Parameters.AddWithValue("@Province", Province.Text);
+            
+           
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+
+            //if ((first != "") & (first != "First Name")) {
+            //    if ((last != "") & (last != "Last Name")) {
+            //        if ((gender != "") & (gender != "Gender")) {
+            //            if ((province != "") & (province != "Province")) {
+
+            //            }
+            //            else {
+            //                MessageBox.Show("Please enter a province");
+            //            }
+            //        }
+            //        else {
+            //            MessageBox.Show("Please enter a gender");
+            //        }
+            //    }
+            //    else {
+            //        MessageBox.Show("Please enter a last name");
+            //    }
+            //}
+            //else {
+            //    MessageBox.Show("Please enter a first name");
+            //}
+        }
+    }
+}
